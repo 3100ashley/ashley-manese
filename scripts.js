60300d22b353159ef68fab0d907b6b8afd2e9940
3100ashley <60107314+3100ashley@users.noreply.github.com>
2021-01-10 18:29:49 -0800

    initial cv

---
+++ scripts.js
@@ -0,0 +1,17 @@
+
+// Typewriter created by Tameem Safi.
+// source: https://github.com/tameemsafi/typewriterjs
+
+var app = document.getElementById('app');
+
+var typewriter = new Typewriter(app, {
+  loop: true,
+  delay: 75,
+});
+
+typewriter
+  .pauseFor(2500)
+  .typeString('a programmer in the making')
+  .pauseFor(300)
+  .deleteChars(10)
+  .start();
